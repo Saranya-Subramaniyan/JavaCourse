38399d36dd94eef5d16525b9f84dd3d71bb1946f
Saranya-Subramaniyan <saranya42002@gmail.com>
2023-12-22 10:47:57 +0530

    add class and object file

--- src/main/java/org/example/classAndObject/ClassAndObject.java
+++ src/main/java/org/example/classAndObject/ClassAndObject.java
@@ -1,2 +1,7 @@
-package org.example.classAndObject;public class ClassAndObject {
+package org.example.classAndObject;
+
+public class ClassAndObject {
+    public static void main(String[] args) {
+
+    }
 }
