983b8ec1e3f09e1707c1fc8092d7fbc367e5d22f
Saranya-Subramaniyan <saranya42002@gmail.com>
2023-12-27 22:40:47 +0530

    Access Modifier

---
+++ src/main/java/org/example/accessModifier/outsidePackage/SubClass.java
@@ -0,0 +1,14 @@
+package org.example.accessModifier.outsidePackage;
+
+import org.example.accessModifier.withinPackage.ProtectedMain;
+
+public class SubClass extends ProtectedMain {
+
+
+    public static void main(String[] args) {
+        ProtectedMain protectedMain=new ProtectedMain();
+
+
+    }
+
+}
