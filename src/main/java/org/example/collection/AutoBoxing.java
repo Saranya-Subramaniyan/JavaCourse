e9e110d36d944f39d3c9d5c2034fe391cdd1a603
Saranya-Subramaniyan <saranya42002@gmail.com>
2023-12-28 16:05:43 +0530

    Collections

---
+++ src/main/java/org/example/collection/AutoBoxing.java
@@ -0,0 +1,10 @@
+package org.example.collection;
+//convert primitive data type to objects
+public class AutoBoxing {
+    public static void main(String[] args) {
+        int i=1220;
+        Integer num=i;
+        System.out.println(num+" "+i);
+
+    }
+}
---
+++ src/main/java/org/example/collection/List/LinkedListCollection.java
@@ -0,0 +1,29 @@
+package org.example.collection.List;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class LinkedListCollection {
+    public static void main(String[] args) {
+        LinkedList<Integer> list=new LinkedList<>();
+        list.add(100);
+        list.add(90);
+        list.add(70);
+        System.out.println(list);
+        list.add(2,80);
+        System.out.println(list);
+        list.addFirst(110);
+        list.addLast(60);
+        System.out.println(list);
+        list.remove();
+        System.out.println(list);
+        System.out.println(list.remove(1)+" "+list+" " +
+                ""+list.removeFirst()+" "+list.removeLast()+" \n"+list);
+        Iterator<Integer> iterato=list.listIterator();
+        while(iterato.hasNext()){
+            System.out.println(iterato.next());
+        }
+    }
+}
