bd6043727aecf10757fcd343a790740a0edaed3e
Saranya-Subramaniyan <saranya42002@gmail.com>
2024-01-04 11:36:38 +0530

    comparator and stream api

---
+++ src/main/java/org/example/streamAPI/StreamAPIBasic.java
@@ -0,0 +1,40 @@
+package org.example.streamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class StreamAPIBasic {
+    public static void main(String[] args) {
+        List<Integer> list= Arrays.asList(1,4,2,3);
+
+    //stream api
+        list.forEach(n-> System.out.println(n));
+
+        //for each work
+/*
+        Consumer<Integer> nums=new Consumer<Integer>() {
+            @Override
+            public void accept(Integer n) {
+                System.out.println(n);
+            }
+        };
+
+
+
+        //use lambda exp
+
+        Consumer<Integer> nums=n->System.out.println(n);
+
+                nums.forEach(n-> System.out.println(n));
+
+
+ */
+
+        int sum=0;
+        for(int n:list){
+           sum+= n%2==0?n*2:n*3;
+        }
+        System.out.println(sum);
+    }
+}
